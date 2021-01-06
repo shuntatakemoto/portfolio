d2a1c669b496ab3e710f71eb8a1e10b0ef15cee3
shuntatakemoto <rt80405@gmail.com>
2021-01-06 20:16:36 +0900

    add folders,files

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Card.css'
+
+class Card extends React.Component{
+    render(){
+      return(
+        <div className='lesson-card'>
+          <div className='lesson-item'>
+            <p></p>
+            <img />
+          </div>
+        </div>
+        )
+    }
+  }
+
+  export default Card;
