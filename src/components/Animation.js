b9459ac2b5b779ce5292923ff59b44b8d7934ba9
shuntatakemoto <rt80405@gmail.com>
2021-01-08 16:54:09 +0900

    球体が回るアニメーション完成

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,59 +1,64 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
-class Animation extends Component{
+import './Animation.css'
+
+export default class ThreeSampleCube extends Component {
+
   componentDidMount(){
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
-    //ADD SCENE
-    this.scene = new THREE.Scene()
-    //ADD CAMERA
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    )
-    this.camera.position.z = 2
-    //ADD RENDERER
-    this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setClearColor('#282c34')
-    this.renderer.setSize(width, height)
-    this.mount.appendChild(this.renderer.domElement)
-    //ADD CUBE
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 'rgb(192, 157, 224)' })
-    this.cube = new THREE.Mesh(geometry, material)
-    this.scene.add(this.cube)
-    this.start()
+  	init()
   }
-  componentWillUnmount(){
-    this.stop()
-    this.mount.removeChild(this.renderer.domElement)
+
+  render() {
+    return (
+       <div className="cube">
+            <canvas className="ThreeSampleCube"></canvas>
+       </div>
+    );
+  };
+}
+
+// プロパティ
+var backgroundColor = "#282c34"
+var cubeColor = 'rgb(192, 157, 224)'
+
+function init() {
+  // シーンを作成
+  const scene = new THREE.Scene();
+  // ジオメトリーを作成
+//   const geometry = new THREE.IcosahedronGeometry( 100, 1 );
+  const geometry = new THREE.SphereGeometry(120, 20, 20);
+  // マテリアルを作成
+  const material = new THREE.MeshBasicMaterial({color: cubeColor, wireframe: true});
+  // メッシュを作成
+  const cube = new THREE.Mesh(geometry, material);
+  // 3D空間にメッシュを追加
+  scene.add(cube);
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, 1.0);
+  camera.position.set(0, 0, +600);
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('.ThreeSampleCube'),
+    antialias: true
+  });
+  renderer.setClearColor( backgroundColor, 1);
+  //回転のアニメーション
+  tick();
+  function tick() {
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.01;
+    requestAnimationFrame(tick);
   }
-  start = () => {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate)
-    }
-  }
-  stop = () => {
-    cancelAnimationFrame(this.frameId)
-  }
-  animate = () => {
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
-    this.renderScene()
-    this.frameId = window.requestAnimationFrame(this.animate)
-  }
-  renderScene = () => {
-    this.renderer.render(this.scene, this.camera)
-  }
-  render(){
-    return(
-      <div
-        style={{ width: '400px', height: '400px' }}
-        ref={(mount) => { this.mount = mount }}
-      />
-    )
+  //リサイズした時の処理
+  onResize();
+  window.addEventListener('resize', onResize);
+  function onResize() {
+    const width = window.innerWidth*0.8;
+    const height = window.innerHeight*0.6;
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
   }
 }
-export default Animation
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,63 +3,8 @@ import React from 'react'
 import './Home.css'
 import { useRef } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
-// import { css, jsx } from '@emotion/core';
-// import { css, jsx } from '@emotion/react';
 
 import Animation from './Animation'
-// class Home extends React.Component {
-//     render(){
-//       return(
-//         <div  className="wrapper">
-//           <div className="home__main">
-//             <div className="home__name">
-//               <div className="home__description">
-//                 <h1>SHUNTA TAKEMOTO</h1>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       )
-//     }
-//   }
-
-// const theme = css`
-//     width: 100vw;
-//     height: 100vh;
-// `;
-
-// HPPのいけたコード３２ー５９
-// const Thing = () => {
-//     const ref = useRef();
-//     useFrame(() => (ref.current.rotation.z += 0.01));
-//     return (
-//         <mesh
-//             ref={ref}
-//             onClick={e => console.log('click')}
-//             onPointerOver={e => console.log('hover')}
-//             onPointerOut={e => console.log('unhover')}
-//         >
-//             <planeBufferGeometry attach='geometry' args={[1, 1]} />
-//             <meshBasicMaterial
-//                 attach='material'
-//                 color='hotpink'
-//                 opacity={0.5}
-//                 transparent
-//             />
-//         </mesh>
-//     );
-// };
-
-// const Home = () => {
-//     return (
-//         <Canvas>
-//             <Thing />
-//         </Canvas>
-//     );
-// };
-
-
-
 
 class Home extends React.Component {
   state = { isVisible: false };
