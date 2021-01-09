26b570f0e1622e113d65073a55aab2baab456809
shuntatakemoto <rt80405@gmail.com>
2021-01-10 03:37:01 +0900

    ok

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,18 +4,20 @@ class Card extends React.Component{
     render(){
        return(
         <div className='card'>
+          <div class="card_image">
             <img
-            className="card__img"
-            src={this.props.image}
-            alt={this.props.name}
-            />
+              className="card__img"
+              src={this.props.image}
+              alt={this.props.name}
+          />
+          </div>
           <div className="card__content">
-            <div className="card__title">
+            <div className="card__name">
               {this.props.name}
-              </div>
-              <div className="card__lead">
+            </div>
+              {/* <div className="card__lead">
               {this.props.lead}
-              </div>
+              </div> */}
           </div>
         </div>
         )
