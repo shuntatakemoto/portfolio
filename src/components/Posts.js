9db1ec9d15bbab8069e96b3a011f83322d0c6a87
shuntatakemoto <rt80405@gmail.com>
2021-01-12 18:23:20 +0900

    rssデザイン完了

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,7 +7,8 @@ import Moment from 'react-moment'
 /*RSSParserのインスタンス生成*/
 const RssParser = require('rss-parser');
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
-const url = CORS_PROXY + 'https://zenn.dev/shuntatakemoto/feed'
+const url1 = CORS_PROXY + 'https://zenn.dev/shuntatakemoto/feed'
+const url2 = CORS_PROXY + 'https://haruta.hatenablog.jp/feed'
 const rssParser = new RssParser();
 
 class Posts extends Component {
@@ -21,7 +22,7 @@ class Posts extends Component {
   }
   /*stateにrssParserの結果をバインド*/
   componentDidMount() {
-    rssParser.parseURL(url)
+    rssParser.parseURL(url1)
       .then((feed) => {
         const data = feed.items;
         console.log(data);
@@ -37,10 +38,18 @@ class Posts extends Component {
     const classes = this.props.classes;
     /*表示するコンテンツの形に合わせてstateの中身を書きだす(mapで拡張forループみたいに使う)*/
     const contents = this.state.contents.map(content => {
-      return <div>
-        {content.title}
-        /*<Moment format="YYYY/MM/DD">{content.pubDate}</Moment>*/
-       /*{content.link}*/
+      return <div className="posts-parent">
+        <div  className="posts-return">
+            <div className="post-date">
+                <Moment format="YYYY/MM/DD">{content.pubDate}</Moment>
+            </div>
+            <div className="post-title">
+                {content.title}
+            </div>
+            <div className="post-link">
+                <a href={content.link}>Read More</a>
+            </div>
+        </div>
       </div>
     });
     return (
